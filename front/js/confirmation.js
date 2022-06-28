5dd13fe42bfba97382f59b3e1de909758e6a62ad
luc laurent <laurentluc7@gmail.com>
2022-06-29 00:40:49 +0200

    remove commentary

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -11,14 +11,3 @@ const displayOrderNumber = () => {
     }, 5000);
 };
 displayOrderNumber();
-
-// let orderParams = new URLSearchParams(document.location.search);
-
-// const displayOrderNumber = () => {
-//     orderId.textContent = orderParams.get("id");
-//     localStorage.clear();
-//     setTimeout(() => {
-//         document.location.href = "file:///Users/macbook/Documents/P5-Dev-Web-Kanap/front/index.html";
-//     }, 5000);
-// };
-// displayOrderNumber();
