0089553e416a9b6a9331b4140231d307106ac985
luc laurent <laurentluc7@gmail.com>
2022-06-29 00:34:32 +0200

    fix bug port

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -7,7 +7,18 @@ const displayOrderNumber = () => {
     orderId.textContent = orderParams.get("id");
     localStorage.clear();
     setTimeout(() => {
-        document.location.href = "http://127.0.0.1:5500/front/html/index.html";
+        document.location.href = "http://127.0.0.1:5501/front/html/index.html";
     }, 5000);
 };
 displayOrderNumber();
+
+// let orderParams = new URLSearchParams(document.location.search);
+
+// const displayOrderNumber = () => {
+//     orderId.textContent = orderParams.get("id");
+//     localStorage.clear();
+//     setTimeout(() => {
+//         document.location.href = "file:///Users/macbook/Documents/P5-Dev-Web-Kanap/front/index.html";
+//     }, 5000);
+// };
+// displayOrderNumber();
